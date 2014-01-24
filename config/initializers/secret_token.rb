efbb274ec227a3faccf3b31a744cd8abdb6d4d0a
julestakahashi <julestakahashi@gmail.com>
2014-01-23 23:45:12 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LbFind::Application.config.secret_key_base = 'be319bde0d404b3272b5ab5aa752a92183f52b90d1c9e2b663e20a7ce2277759433b43abe8ff7f6e16b1339cbd15629d93c8c285942d59d4ed40d69cc450c62d'
