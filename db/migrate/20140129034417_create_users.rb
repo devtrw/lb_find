affb25eafb22a17e369bdf263f6aefc286f11aea
Steven Nance <steven@devtrw.com>
2014-01-28 20:05:10 -0800

    fix user.username migration definition

--- db/migrate/20140129034417_create_users.rb
+++ db/migrate/20140129034417_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.int :age
+      t.integer :age
       t.string :location
       t.string :discipline
       t.timestamps
