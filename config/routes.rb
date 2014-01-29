ac6ae720c38ec76700822cdb72b9b9ab2e696666
Steven Nance <steven@devtrw.com>
2014-01-28 21:30:54 -0800

    add route for /api/users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LbFind::Application.routes.draw do
   get "api/longboarder"
+  get "api/users"
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
