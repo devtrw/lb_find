4e3728e58357edebafc8b6f1c40e85e149f3ba69
julestakahashi <julestakahashi@gmail.com>
2014-02-05 23:04:19 -0800

    update home.js file and add mapmarker.svg

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,22 +3,88 @@ function initialize() {
         style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
     };
     var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
+        center: new google.maps.LatLng(40.000, -100.644),
         mapTypeControlOptions: controlOptions,
-        zoom: 8
+        zoom: 4
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    jQuery(document).ready(function () {
+        var count = 0;
+        var eyes = jQuery('#logo').find('.eyes');
+
+        function change_eye_color() {
+            setTimeout(function () {
+                var eye_color = (count % 2) ? '#f00' : '#0f0';
+                eyes.css('background-color', eye_color);
+                count++;
+                change_eye_color();
+            }, 1000);
+        }
+
+        change_eye_color();
+
+        function draw_map_marker(map_coordinates) {
+            // return google marker object
+            var reddit_longboarder = 'assets/mapmarker.svg';
+            var marker = new google.maps.Marker({
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: reddit_longboarder,
+                position: map_coordinates
+
+            })
+
+            return marker;
+        }
+
+        function process_longboarder_location(google_api_result) {
+            var map_coordinates = google_api_result[0].geometry.location;
+            return draw_map_marker(map_coordinates);
+        }
+
+        function lookup_longboarder_location(longboarder) {
+            // return google LatLng object
+           // console.log(location)
+           geocoder = new google.maps.Geocoder();
+           geocoder.geocode({address: longboarder.location},  function (result, status) {
+               var marker = process_longboarder_location(result)
+               var infoContent = '<h1>' + longboarder.username + '<br>' + longboarder.age + '<br>' +'</h1>'
+               infoContent += '<ul>'
+               for (var i = 0; i < longboarder.discipline.length; i++ ){
+                   infoContent += '<li>discipline number ' + i + ': ' + longboarder.discipline[i] + '</li>';
+               }
+               infoContent += '</ul>'
+               var infowindow = new google.maps.InfoWindow({
+                   content: infoContent
+
+               });
+
+               google.maps.event.addListener(marker, 'click', function() {
+                   infowindow.open(map,marker);
+               });
+           });
+        }
+
+        function add_longboarder_to_map(longboarder) {
+           // console.info('adding longboarder(/u/' + longboarder.username + ') from ' + longboarder.location + ' to map');
+            lookup_longboarder_location(longboarder);
+
+        }
+
+        function parse_api_result(parsed_result) {
+            var long_boarders = parsed_result.longboarders;
+            var long_boarder_count = long_boarders.length;
+
+            for (var i = 0; i < long_boarder_count; i += 1) {
+                lookup_longboarder_location(long_boarders[i]);
+            }
+        }
+
+        jQuery.get('/api/longboarder', parse_api_result, 'json');
+    });
+
 }
+
+
 google.maps.event.addDomListener(window, 'load', initialize);
-$(function () {
-    var count = 0, eyes = $('#logo').find('.eyes');
-    function change_eye_color() {
-        setTimeout(function () {
-            var eye_color = (count % 2) ? '#f00' : '#0f0';
-            eyes.css('background-color', eye_color);
-            count++;
-            change_eye_color();
-        }, 1000);
-    }
-    change_eye_color();
-});
