074161241c12944b051569a10cf512d8ba4ae5ad
julestakahashi <julestakahashi@gmail.com>
2014-02-13 20:14:08 -0800

    generate viewfiles for devise

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -69,9 +69,19 @@ function initialize() {
         }
 
         jQuery.get('/api/longboarder', parse_api_result, 'json');
-    });
+
+
 
 }
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+//facebook
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
