4b18c426fbf70330778a7e66ba79831170431a35
Sam Britton <dasclockwork@gmail.com>
2014-02-18 13:45:07 -0800

    remove alien

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,7 +8,7 @@ class ApiController < ApplicationController
           { location: 'San Jose, California', discipline: [:freeride], username: 'ihaveachicken', age: 16 },
           { location: 'Sydney, Australia', discipline: [:dancing], username: 'longboard4laif', age: 32 },
           { location: 'NewYork, NewYork', discipline: [:cruising], username: 'evanli123', age: 1 },
-          { location: '97213', discipline: [:cruising, :dancing], username: 'pdx_longboarder_420', age: 93 },
+          { location: '97219', discipline: [:cruising, :dancing], username: 'pdx_longboarder_420', age: 93 },
           { location: 'Austin, TX', discipline: [:downhill], username: 'thane-lines', age: 2 },
           { location: 'LosAngeles, CA', discipline: [], username: 'MrFrodoo', age: 43 },
         ]
